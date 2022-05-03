6ce64a34c4da0a9edc52d664896750db5098451e
Matt McAdams <mattgmcadams@gmail.com>
2022-05-03 16:48:47 -0400

    add data

--- src/daq1.py
+++ src/daq1.py
@@ -70,28 +70,18 @@ while gamepad.isConnected():
 			sleep(2)
 			break
 
-while True:
+
+for _ in range(20000):
+
 
 	if start:
 
-		# Read video, extract and return the frame
-		# ret0, img0 = video0.read()
-		# ret4, img4 = video4.read()
+		cv2.imshow('inspirational image', img0)
 
-		# concatenate images Vertically
-		# Verti = np.concatenate((img0, img4), axis=0)
-
-		# Display the image
-		cv2.imshow('live video', img0)
-
-
-		# wait for user to press any key
-		key = cv2.waitKey(5)
-
+		#fafafaf wait for user to press any key
+		key = cv2.waitKey(2)
 		binary = serL.readline()
-
 		data = str(binary).split(':')
-
 		try:
 			[ax, ay, az, gx, gy, gz, mx, my, mz, tbs] = str(bin).split(':')
 			trash = 'none'
@@ -100,31 +90,6 @@ while True:
 		except:
 			[ax, ay, az, gx, gy, gz, mx, my, mz, tbs] = None, None, None, None, None, None, None, None, None, None
 			trash = data
-
-
-
-
-		# try:
-		# 	ax = float(_ax)
-		# 	ay = float(_ay)
-		# 	az = float(_az)
-		# except:
-		# 	ax, ay, az = None, None, None
-		#
-		# try:
-		# 	gx = float(_gx)
-		# 	gy = float(_gy)
-		# 	gz = float(_gz)
-		# except:
-		# 	gx, gy, gz = None, None, None
-		#
-		# try:
-		# 	mx = float(_mx)
-		# 	my = float(_my)
-		# 	mz = float(_mz)
-		# except:
-		# 	mx, my, mz = None, None, None
-
 
 		b0, b1, b2, b3 = 0, 0, 0, 0
 
@@ -148,61 +113,13 @@ while True:
 			b3 = 1
 
 
-		# Determine the type
-		# if eventType == 'BUTTON':
-		# 	# Button changed
-		# 	if control == button0:
-		# 		# Trigger
-		# 		if value:
-		# 			print('b0 hit')
-		# 			b0 = 1
-		#
-		# 	elif control == button1:
-		# 		# Left
-		# 		if value:
-		# 			print('b1 hit')
-		# 			b1 = 1
-		#
-		# 	elif control == button3:
-		# 		if value:
-		# 			print('b3 hit')
-		# 			b2 = 1
-		#
-		# 	elif control == button2:
-		# 		if value:
-		# 			print('b2 hit')
-		# 			b3 = 1
-		#
-		# 	elif control == buttonExit:
-		# 		# Exit
-		# 		if value:
-		# 			start = False
-		# 			break
-
-
-		# filename0 = f'cam0_{str(i)}.jpg'
-		# filename1 = f'cam1_{str(i)}.jpg'
-
 		ts2 = time.time()# - ts1
 
-		# Save the images in given path
-		# cv2.imwrite(filename0, img0)
-		# cv2.imwrite(filename1, img4)
-		# i = i+1
-
-		metadf0.loc[len(metadf0.index)] = [ts2, b0, b1, b2, b3, axL, ayL, azL, gxL, gyL, gzL, mxL, myL, mzL, tbsL, trash]
-		# metadf1.loc[len(metadf1.index)] = [ts2, b0, b1, b2, b3, axL, ayL, azL, gxL, gyL, gzL, mxL, myL, mzL, tbsL, axR, ayR, azR, gxR, gyR, gzR, mxR, myR, mzR, tbsR, trash]
-
-# # close the camera
-#video0.release()
-# video4.release()
+		metadf0.loc[len(metadf0.index)] = [ts2, b0, b1, b2, b3, ax, ay, az, gx, gy, gz, mx, my, mz, tbs, trash]
 
 # close open windows
 cv2.destroyAllWindows()
 
 # save meta to csv
-# metadf_all = pd.concat([metadf0, metadf1], ignore_index=True)
 metadf0.to_csv('meta.csv', index=False)
-# metadf1.to_csv('meta_cam1.csv', index=False)
-# metadf_all.to_csv('meta.csv', index=False)
 
